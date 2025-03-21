908282513d4cc275dc43c31c313b6e33483fcadd
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2025-03-21 15:23:18 +0700

    update: like fitur

--- src/services/postgres/UserAlbumLikesService.js
+++ src/services/postgres/UserAlbumLikesService.js
@@ -9,19 +9,31 @@ class UserAlbumLikesService {
     this._pool = new Pool();
     this._albumsService = new AlbumsService();
   }
+  async isLiked(albumId, userId) {
+    const query = {
+      text: 'SELECT 1 FROM user_album_likes WHERE album_id = $1 AND user_id = $2',
+      values: [albumId, userId],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows.length > 0;
+  }
 
   async likeAlbum(albumId, userId) {
     await this._albumsService.verifyAlbumExist(albumId);
 
-    const id = `like-${nanoid(16)}`;
+    const isAlreadyLiked = await this.isLiked(albumId, userId);
+    if (isAlreadyLiked) {
+      throw new InvariantError('Anda sudah like album ini');
+    }
 
+    const id = `like-${nanoid(16)}`;
     const query = {
-      text: 'INSERT INTO user_album_likes VALUES($1, $2, $3) RETURNING id',
+      text: 'INSERT INTO user_album_likes (id, user_id, album_id) VALUES($1, $2, $3) RETURNING id',
       values: [id, userId, albumId],
     };
 
     const result = await this._pool.query(query);
-
     if (!result.rows.length) {
       throw new InvariantError('Gagal like album');
     }
